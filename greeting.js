d1fb57c6ca60a36ac156c24374e0084a96be1f50
Sangpil Jung <tankteam20@gmail.com>
2021-01-18 10:00:48 +1000

    Todos

--- greeting.js
+++ greeting.js
@@ -2,22 +2,16 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
+//console.log(form,input)
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
-                        // text is an argument 
-function paintGreeting(text){
-    form.classList.remove(SHOWING_CN); // block showing form 
-    greeting.classList.add(SHOWING_CN); // show me the  h4 greeting 
-    greeting.innerText = `Hello ${text}`; // put text i sent him 
-}
 
 function saveName(text){
-    localStorage.setItem(USER_LS, text);
+    localStorage.setItem(USER_LS,text);
 }
 
 function handleSubmit(event){
-    event.preventDefault()
-    // make it nothing happen when press enter 
+    event.preventDefault();
     const currentValue = input.value;
     //console.log(currentValue);
     paintGreeting(currentValue);
@@ -26,25 +20,24 @@ function handleSubmit(event){
 
 function askForName(){
     form.classList.add(SHOWING_CN);
-    form.addEventListener("submit",handleSubmit);
+    form.addEventListener("submit", handleSubmit);
 }
-//step two showing the form by using form.classList.add(SHOWING_CN) 
-// form (document) . find class list . action add (object)
-// form (document) go to 'form' document and do / eventListener / ("submit")
-// however on the website it looks refresh because it does not send to user's local storage 
-//so add handleSubmit() function which is doing default behavior which sent input data to the "root"
 
-
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN)
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`
+}
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
+    //console.log(currentUser)
     if(currentUser === null){
-        // user is not here
+        //no exist
         askForName();
-        //step one add askForname() function 
     }else{
-        //she is here 
-        paintGreeting(currentUser); 
+        // exsit
+        paintGreeting(currentUser);
     }
 }
 
--- todo.js
+++ todo.js
@@ -1,45 +1,36 @@
-// step one 
 const toDoForm = document.querySelector(".js-toDoForm"),
-    toDoInput = toDoForm.querySelector("input");
+    toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
-    //step four 
 const TODOS_LS = 'toDos';
 
-
-// step seven
 function paintToDo(text){
-    //console.log(text);
-    // to show the .value dont need to go back to html create "li" to create new element 
-    const li = document.createElement("li");
+    const li = document.childElementCount("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "✘";
+    delBtn.value = "✘";
     const span = document.createElement("span");
-    span.innerText = text
+    span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
     toDoList.appendChild(li);
 }
-//step six
+
 function handleSubmit(event){
     event.preventDefault();
-    const currentValue = toDoInput.value;
+    const currentValue = input.value;
     paintToDo(currentValue);
-    // after execute paintToDo and make form look default creating code below.
-    toDoInput.value = "";
+    toDoInput.value = ""; 
 }
- 
-// step three 
+
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS)
+    const toDos = localStorage.getItem(TODOS_LS);
     if(toDos !== null){
+        
     }
 }
 
-// step two 
 function init(){
     loadToDos();
-    // five
     toDoForm.addEventListener("submit",handleSubmit);
 }
 
