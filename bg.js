3b78c9b5cc1579b396bd5f66b35852e937845fe1
Sangpil Jung <tankteam20@gmail.com>
2021-01-20 12:26:50 +1000

    background randome img generator

--- bg.js
+++ bg.js
@@ -2,19 +2,21 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
-function paintIMG(imgNumber){
+function paintImg(imgNumber){
     const image = new Image();
-    img.src = '/imgs/'
+    image.src = `imgs/${imgNumber + 1}.jpg`;
+    image.classList.add('bgImage');
+    body.prepend(image);
 }
 
 function genRandom(){
-    const number = Math.floor(Math.random() * 3);
-    return number;
+    const number = Math.floor(Math.random()*3);
+    return number
 }
 
 function init(){
     const randomNumber = genRandom();
-    paintIMG(randomNumber )
-}
+    paintImg(randomNumber);
+};
 
-init();
+init()
